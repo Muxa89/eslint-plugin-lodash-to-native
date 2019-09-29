b07a47824890f1027bcc74f1c2c8b522cab68166
Muxa89 <muxa.89@inbox.ru>
2019-09-29 17:19:17 +0400

    Added some tests.

--- tests/lib/rules/map.js
+++ tests/lib/rules/map.js
@@ -3,35 +3,31 @@
 const rule = require("../../../lib/rules/map.js");
 const RuleTester = require("eslint").RuleTester;
 
-const ruleTester = new RuleTester({
-});
+const ruleTester = new RuleTester({});
 
 ruleTester.run("map", rule, {
   valid: [
-    // '_.map()',
-    // '(Array.isArray(arr)) ? arr.map(fn) : _.map(arr, fn);'
-    // '_.notMap()',
-    // '_.map(123)',
-    // '_.map([1, 2, 3])',
-    // 'var a = []; _.map();',
-    // {
-    //   code: '_.map()',
-    // }
+    "_ = function() {}; _.map([1, 2, 3], fn);",
+    "_.map({a: 1, b: 2}, fn);"
   ],
 
   invalid: [
     {
-      code: "_.map([1, 2, 3], function() {}); _ = fn(); _.map(arr, fn);",
+      code: "_.map(arr, fn);",
       errors: 1,
-      // output: "123;"
+      output: "(Array.isArray(arr)) ? arr.map(fn) : _.map(arr, fn);"
+    }, {
+      code: "var _ = require('lodash'); _.map([1, 2, 3], fn);",
+      errors: 1,
+      output: "var _ = require('lodash'); [1, 2, 3].map(fn);"
+    }, {
+      code: "var _ = require('lodash'); _.map([1, 2, 3], fn); _ = function() {}; _.map([3, 4], fn2);",
+      errors: 1,
+      output: "var _ = require('lodash'); [1, 2, 3].map(fn); _ = function() {}; _.map([3, 4], fn2);"
+    }, {
+      code: "_.map([1, 2, 3], fn);",
+      errors: 1,
+      output: "[1, 2, 3].map(fn);"
     }
-    // {
-    //   code: "var invalidVariable = true",
-    //   errors: [{ message: "Unexpected invalid variable." }]
-    // },
-    // {
-    //   code: "var invalidVariable = true",
-    //   errors: [{ message: /^Unexpected.+variable/ }]
-    // }
   ]
 });
